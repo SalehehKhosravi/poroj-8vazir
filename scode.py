abd545ac92edeb53936f17cfbb1b215126e5a3d6
SalehehKhosravi <salehehkhosravi8@gmil.com>
2025-04-27 10:07:08 +0330

    add calculate_fitness def baray mohasebeh barazandegii to scode.py

--- scode.py
+++ scode.py
@@ -8,3 +8,12 @@ class EightQueensGA:
       def initialize_population(self):
         """ایجاد جمعیت اولیه به صورت تصادفی."""
         return [self.random_solution() for _ in range(self.population_size)]
+      
+      def calculate_fitness(self, solution):
+        """محاسبه برازندگی یک راه‌حل."""
+        clashes = 0      #شمارش تداخل ها
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                if solution[i] == solution[j] or abs(solution[i] - solution[j]) == j - i: #بررسی تداخل
+                    clashes += 1
+        return 28 - clashes  # برازندگی حداکثر 28 برای 8 وزیر
