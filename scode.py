dfba7780858f2a070594d60116f03ae86699026f
SalehehKhosravi <salehehkhosravi8@gmil.com>
2025-04-27 10:04:38 +0330

    add random_solution def baray ejade rahehale tasadofii to scode.py

--- scode.py
+++ scode.py
@@ -1,4 +1,10 @@
 class EightQueensGA:
-     def initialize_population(self):
+     
+      def random_solution(self):    #لیستی از موقعیت وزیرها    
+        """ایجاد یک راه‌حل تصادفی."""
+        return random.sample(range(self.n), self.n)
+
+
+      def initialize_population(self):
         """ایجاد جمعیت اولیه به صورت تصادفی."""
         return [self.random_solution() for _ in range(self.population_size)]
